3fb4172ae41feb190ed50d957e1517c6656e926b
Christoph Lechner <cl@cl>
2026-02-09 23:41:06 +0100

    added parameter to URL to request sorting of data

--- test_stadtrad1.py
+++ test_stadtrad1.py
@@ -8,7 +8,11 @@ import matplotlib.pyplot as plt
 
 # Infos about parameters in the request:
 # https://fraunhoferiosb.github.io/FROST-Server/sensorthingsapi/requestingData/STA-Tailoring-Responses.html
-url = 'https://iot.hamburg.de/v1.0/Datastreams?$count=true&$filter=properties/serviceName%20eq%20%27HH_STA_StadtRad%27%20and%20properties/layerName%20eq%20%27Fahrraeder%27&$expand=Observations($select=phenomenonTime,result;$orderby=phenomenonTime%20desc;$top=10)'
+# URL copied from web browser
+# url = 'https://iot.hamburg.de/v1.0/Datastreams?$count=true&$filter=properties/serviceName%20eq%20%27HH_STA_StadtRad%27%20and%20properties/layerName%20eq%20%27Fahrraeder%27&$expand=Observations($select=phenomenonTime,result;$orderby=phenomenonTime%20desc;$top=10)'
+#
+# The original URL does not define the sort order of the objects. As consequence as we navigate through the dataset using the "@iot.nextLink" (retrieving 100 objects at a time), we get some objects multiple times while not getting other objects even a single time
+url = 'https://iot.hamburg.de/v1.0/Datastreams?$count=true&$orderBy=name&$filter=properties/serviceName%20eq%20%27HH_STA_StadtRad%27%20and%20properties/layerName%20eq%20%27Fahrraeder%27&$expand=Observations($select=phenomenonTime,result;$orderby=phenomenonTime%20desc;$top=10)'
 
 stations=[]
 coords=[]
@@ -25,7 +29,7 @@ while url:
     #    url = data['@iot.nextLink']
 
     # Loop over all stations in the dataset
-    with open(f'log_{cntr}.txt','w') as fout:
+    with open(f'out/log_{cntr}.txt','w') as fout:
         for station in data['value']:
             # print(f"{station['@iot.id']} {station['observedArea']['coordinates'][0]},{station['observedArea']['coordinates'][1]}")
             coords.append(station['observedArea']['coordinates'])
