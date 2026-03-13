19cfb935ac62d8b081f59eb5d7fa1a251cf596db
Christoph Lechner <cl@cl>
2026-03-13 11:06:34 +0100

    functions for weather data (API req./processing) now in extra file

--- loader_weatherdata.py
+++ loader_weatherdata.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python3
 
 import psycopg
-import requests
 import datetime
 import gzip
 from functools import partial
 from pathlib import Path
+from weatherdata import get_weather_data,process_weather_data
 from db_conn import get_db_conn
 
 
 datadir = Path('data/')
-
-# Coordinates for Hamburg, Germany
-lat, lon = 53.55, 10.00
-
 
 def prepare_stg_table(cur, stg_table):
     # temporary staging table, having same schema as destination of MERGE operation (see schema.sql)
@@ -47,80 +43,6 @@ def prepare_stg_table(cur, stg_table):
         );
         """
     )
-
-def get_data(str_date=None, my_cb_store=None):
-    if str_date is None:
-        str_date = datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%d')
-
-    # here it will always be one file, but using list so that returned data structure is identical to function for bike traffic data
-    files = []
-
-
-    # print(str_date)
-    params = {
-        'date': str_date,
-        'lat': lat,
-        'lon': lon,
-    }
-    url = 'https://api.brightsky.dev/weather'
-    response = requests.get(url, params=params)
-    response.raise_for_status() # exception when HTTP status code is 4xx or 5xx
-
-    if my_cb_store:
-        fn_without_path = my_cb_store(response=response) # only argument: response data structure (all others are assigned in caller function)
-        files.append(fn_without_path)
-    data = response.json()
-    datasets = [data]
-    return (files,datasets)
-
-
-def process_data(data, cb_row=None):
-    # determine source describing actual measurements
-    meas_source_ids=[]
-    for curr_source in data['sources']:
-        if curr_source['observation_type'] in ['current','historical']:
-            meas_source_ids.append(curr_source['id'])
-
-    # field names in DB
-    db_fields = [
-        'timestamp', 'source_id', 'precipitation', 'pressure_msl', 'sunshine', 'temperature', 'wind_direction', 'wind_speed', 'cloud_cover', 'dew_point', 'relative_humidity', 'visibility', 'wind_gust_direction', 'wind_gust_speed', 'precipitation_probability', 'precipitation_probability_6h', 'solar',
-        #
-        # According to brightsky.de documentation, these fields are "not taken as-is from the raw data (because it does not exist)". They are calculated from different fields in the raw data as a best effort.
-        'condition', 'icon'
-    ]
-    # field names in brightsky.dev JSON response
-    src_fields = [
-        'timestamp', 'source_id', 'precipitation', 'pressure_msl', 'sunshine', 'temperature', 'wind_direction', 'wind_speed', 'cloud_cover', 'dew_point', 'relative_humidity', 'visibility', 'wind_gust_direction', 'wind_gust_speed', 'precipitation_probability', 'precipitation_probability_6h', 'solar',
-        #
-        # According to brightsky.de documentation, these fields are "not taken as-is from the raw data (because it does not exist)". They are calculated from different fields in the raw data as a best effort.
-        'condition', 'icon'
-    ]
-
-
-    all_db_fields = ['query_lat','query_lon'] # ts_entry_creation will be set automatically
-    all_db_fields.extend(db_fields)
-    N_all_db_fields = len(all_db_fields)
-    q_all_db_fields = ','.join(all_db_fields)
-    q_all_db_fields_placeholders = ','.join(['%s']*N_all_db_fields)
-
-    ndata=0
-    for curr_m in data['weather']:
-        # we want only actual measurements (historical data is accepted as well, it has different id) -> reject any forecasts, etc.
-        if not (curr_m['source_id'] in meas_source_ids):
-            continue
-
-        values=[lat,lon]
-        for k in src_fields:
-            values.append(curr_m[k])
-        # print(values)
-        if cb_row:
-            cb_row(
-                q_all_db_fields=q_all_db_fields,
-                q_all_db_fields_placeholders=q_all_db_fields_placeholders,
-                values=values
-            )
-        ndata+=1
-    return ndata
 
 def data_merge(cur, stg_table):
     data_table = 'bikeproj_weather'
@@ -212,7 +134,7 @@ def main():
 
         str_date = (t0+datetime.timedelta(days=delta_days)).strftime('%Y-%m-%d')
         print(f'Obtaining weather data for {str_date} (in timezone UTC) ...')
-        files,datasets = get_data(
+        files,datasets = get_weather_data(
                 str_date=str_date,
                 # all sub-requests (for different days) belong together, therefore: same date and incrementing sequence number
                 my_cb_store=partial(cb_store, tstartreq=tstart, seqcntr=loop_cntr)
@@ -228,7 +150,7 @@ def main():
         my_row_cb = partial(process_data_cb_sqlinsert, cur=cur, stg_table=my_stg_table)
         nloaded = 0
         for data in s['datasets']:
-            nloaded += process_data(data, cb_row=my_row_cb)
+            nloaded += process_weather_data(data, cb_row=my_row_cb)
 
         nmerged = data_merge(cur, my_stg_table)
         print(f'loaded:{nloaded} merged:{nmerged}')
